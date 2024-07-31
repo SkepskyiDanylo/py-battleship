980ff13cbc2f98187997ce5ad53b6a8c0d0d3a41
Danylo Skepskyi <kol230305@gmail.com>
2024-07-31 16:52:24 +0200

    Added some comÑÑÑments

--- app/main.py
+++ app/main.py
@@ -80,6 +80,7 @@ class Battleship:
             print("")
 
     def _validate_input(self, ships: list[tuple]) -> None:
+        # Check if count of ships == 10
         if len(ships) < 10:
             raise ValueError(f"Count of ships should be"
                              f"equal 10, got {len(ships)} instead")
@@ -90,9 +91,10 @@ class Battleship:
             4: 0,
         }
         list_of_ships = list(self.field.values())
+        # Counting each ship type, and adding it to dict
         for ship in set(list_of_ships):
             count_of_ships[list_of_ships.count(ship)] += 1
-
+        # Checking if there is correct count of each ship type
         if ([count_of_ships[1], count_of_ships[2],
              count_of_ships[3], count_of_ships[4]] != [4, 3, 2, 1]):
             raise ValueError(f"There should be: \n"
@@ -104,6 +106,7 @@ class Battleship:
                              f"got {count_of_ships[3]} instead\n"
                              f"1 four-deck ship, "
                              f"got {count_of_ships[4]} instead")
+        # Checking if there ships in neighboring cells
         for cord, ship in self.field.items():
             for row in range(cord[0] - 1, cord[0] + 2):
                 for column in range(cord[1] - 1, cord[1] + 2):
